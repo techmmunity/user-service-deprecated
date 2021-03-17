ecb05d03b374d6e7d3107986ad3e2f648c0ef5ae
Henrique Leite <henriqueleite616@gmail.com>
2021-03-17 15:43:44 -0300

    Fix TermsAndPolicies Imports

--- src/api/terms-and-policies/terms-and-policies.module.ts
+++ src/api/terms-and-policies/terms-and-policies.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { TermsAndPoliciesService } from "./terms-and-policies.service";
 
 import { TermsAndPoliciesController } from "./terms-and-policies.controller";
 
+import { TermsAndPoliciesEntity } from "./terms-and-policies.entity";
+
 @Module({
+	imports: [TypeOrmModule.forFeature([TermsAndPoliciesEntity])],
 	providers: [TermsAndPoliciesService],
 	controllers: [TermsAndPoliciesController],
 })
